321099792cc29c02379d81a0563fa9eb6d5f3a5a
Iliès <islim1@myges.fr>
2022-07-21 20:23:27 +0200

    fix footer css + form contact

--- www/View/contact.view.php
+++ www/View/contact.view.php
@@ -1,3 +1,24 @@
-<form>
-    <input>
-</form>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="../dist/main.css">
+</head>
+<body>
+<header id="site-header">
+    <div class="container">
+        <form action="POST">
+        <h1>Formulaire de contact</h1>
+        <label for="nom">Nom</label> <br> <br>
+        <input type="name" name="Nom"> <br> <br>
+        <label for="mail">Mail</label> <br> <br>
+        <input type="mail" name="Mail"> <br> <br>
+        <label for="message">Message</label> <br> <br>
+        <textarea type="message" name="message"></textarea> <br> <br>
+        <button type="button">Valider</button>
+    </form>
+    </div>
+</header>
+</body>
+</html>
